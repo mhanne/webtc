9c4be761bec4db9498781d2acc60e8006e7d9a75
Marius Hanne <marius.hanne@sourceagency.org>
2011-05-29 17:10:42 +0200

    let user change password (but not email address)

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@ class User < ActiveRecord::Base
 
   before_create :check_bitcoin_account
   after_create :create_bitcoin_address
+  before_update :check_address_not_changed
 
   def check_bitcoin_account
     if BITCOIN.getaddressesbyaccount(email).size > 0
@@ -20,12 +21,14 @@ class User < ActiveRecord::Base
     end
   end
 
-
   def create_bitcoin_address
     address = BITCOIN.getaccountaddress(email)
     Address.create(:user => self, :address => address, :label => email)
     update_attribute :address, address
   end
 
+  def check_address_not_changed
+    errors.add("email", :may_not_be_changed)  if email_changed?
+  end
 
 end
