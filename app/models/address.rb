9e709f190a729c9219682e5fbb53386c719bb925
Marius Hanne <marius.hanne@sourceagency.org>
2011-06-05 19:12:06 +0200

    validate address uniqueness in scope of user

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,7 +3,7 @@ class Address < ActiveRecord::Base
   belongs_to :user
 
   validates_presence_of :address
-  validates_uniqueness_of :address
+  validates_uniqueness_of :address, :scope => :user_id
 
   def self.get address
     Address.find_by_address(address) || Address.new(:address => address)
